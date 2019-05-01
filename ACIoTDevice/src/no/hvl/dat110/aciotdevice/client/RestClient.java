a9d4f8a06cccec3d55ffd97505b5f618303024d2
jhoffis <31273598+jhoffis@users.noreply.github.com>
2019-05-01 22:08:55 +0200

    Poppitiscoop

--- ACIoTDevice/src/no/hvl/dat110/aciotdevice/client/RestClient.java
+++ ACIoTDevice/src/no/hvl/dat110/aciotdevice/client/RestClient.java
@@ -14,12 +14,60 @@ public class RestClient {
 	public RestClient() {
 	}
 
-	private static String logpath = "/accessdevice/log";
+	private static String logpath = "/accessdevice/log/";
 
 	public void doPostAccessEntry(String message) {
 
 		// TODO: implement a HTTP POST on the service to post the message
+		try (Socket s = new Socket(Configuration.host, Configuration.port)) {
+			
+			
+			// construct the HTTP request
+			Gson gson = new Gson();
+			String jsonbody = gson.toJson(new AccessMessage(message));
 
+			String httpputrequest = "POST " + logpath + " HTTP/1.1\r\n" + "Host: " + Configuration.host + "\r\n"
+					+ "Content-type: application/json\r\n" + "Content-length: " + jsonbody.length() + "\r\n"
+					+ "Connection: close\r\n" + "\r\n" + jsonbody + "\r\n";
+
+			// send the response over the TCP connection
+			OutputStream output = s.getOutputStream();
+
+			PrintWriter pw = new PrintWriter(output, false);
+			pw.print(httpputrequest);
+			pw.flush();
+
+			// read the HTTP response
+			InputStream in = s.getInputStream();
+
+			Scanner scan = new Scanner(in);
+			StringBuilder jsonresponse = new StringBuilder();
+			boolean header = true;
+
+			while (scan.hasNext()) {
+
+				String nextline = scan.nextLine();
+
+				if (header) {
+					System.out.println(nextline);
+				} else {
+					jsonresponse.append(nextline);
+				}
+
+				if (nextline.isEmpty()) {
+					header = false;
+				}
+
+			}
+
+			System.out.println("BODY:");
+			System.out.println(jsonresponse.toString());
+
+			scan.close();
+
+		} catch (IOException ex) {
+			System.err.println(ex);
+		}
 	}
 
 	private static String codepath = "/accessdevice/code";
@@ -28,7 +76,7 @@ public class RestClient {
 
 		AccessCode code = null;
 
-		// TODO: implement a HTTP GET on the service to get current access code
+		// implement a HTTP GET on the service to get current access code
 
 		try (Socket s = new Socket(Configuration.host, Configuration.port)) {
 			// construct get request
@@ -67,9 +115,9 @@ public class RestClient {
 
 			}
 			Gson gson = new Gson();
-			
+
 			code = gson.fromJson(jsonresponse.toString(), AccessCode.class);
-			
+
 			scan.close();
 
 		} catch (IOException ex) {
